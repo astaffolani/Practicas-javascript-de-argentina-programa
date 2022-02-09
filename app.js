3d0be362329965ac8216ceaa7ee955f81fa4b216
astaffolani <adrian@staffolani.com.ar>
2022-02-09 18:54:20 -0300

    agregue nuevos archivos a practicas

---
+++ app.js
@@ -0,0 +1,1 @@
+document.write("<h1>hola mundo</h1>");
---
+++ condicionales.js
@@ -0,0 +1,6 @@
+let helado = 'chocolate';
+if (helado === 'chocolate') {
+  alert('¡Sí, amo el helado de chocolate!');
+} else {
+  alert('Awwww, pero mi favorito es el de chocolate...');
+}
