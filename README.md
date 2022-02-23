90f5e10e21da19d48845b502a0a847f2b6389800
astaffolani <adrian@staffolani.com.ar>
2022-02-22 22:36:11 -0300

    hola

--- README.md
+++
@@ -1,1 +0,0 @@
-practicas del curso de Argentina programa, cursos que estoy realizando en udemy y  coderhouse y practicas en forma autodidacta
--- condicionales.js
+++
@@ -1,6 +0,0 @@
-let helado = 'chocolate';
-if (helado === 'chocolate') {
-  alert('¡Sí, amo el helado de chocolate!');
-} else {
-  alert('Awwww, pero mi favorito es el de chocolate...');
-}
